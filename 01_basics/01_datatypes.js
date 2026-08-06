a10ed9703a7abe861571400ca6e287e81b0fc52d
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-06 17:07:06 +0530

    Symbol datatype

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -3,6 +3,8 @@ let age = 21
 let isAdult = true
 let married ;
 
+// symbol is also a datatype used for defining uniqueness
+
 console.log(typeof"Gudiya")
 console.log(typeof age)
 console.log(typeof married)
