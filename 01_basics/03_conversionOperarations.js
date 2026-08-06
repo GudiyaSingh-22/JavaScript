9885fc85caa71ea09e3527a37af1f1331af257a5
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-06 20:07:46 +0530

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperarations.js
@@ -0,0 +1,50 @@
+//Conversion of String to No.
+
+// let score = "33abc"// if only "33" then 33 will be returned and if "33abc" then NaN means not A no will be returned
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueScore = Number
+// console.log(valueScore)
+
+
+// let valueOfNo = Number(score)
+// console.log(typeof valueOfNo)
+// console.log(valueOfNo)
+
+//Output
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false=> 0
+
+
+
+// //Conversion of a no to string
+
+// let marks = 98
+// console.log(typeof marks)
+
+// let strMarks = String(marks)
+// console.log(typeof strMarks)
+// console.log(strMarks)
+
+
+
+// // Conversion of string to boolean
+
+// let Subject = "Govind" // if string is empty then it prints false otherwise true.
+// console.log(typeof Subject)
+
+// let boolSubject = Boolean(Subject)
+// console.log(typeof boolSubject)
+// console.log(boolSubject)
+
+
+
+// Conversion from null to boolean
+let Compart = null
+console.log(typeof Compart) // object type
+
+let boolCompart = Boolean(Compart)
+console.log(typeof boolCompart) // boolean type
+console.log(boolCompart) // return false
