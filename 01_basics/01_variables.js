dde1479ccf76c3f20c84cb5319d9c87250a536b4
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-06 15:56:51 +0530

    Learning about const, let and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = "2234"
+//accountId = "45"
+//console.log(accountId);
+
+let accountEmail = "gudiya@gmail.com"
+var accountPsw = 1234
+accountCity = "Jaunpur"
+
+ let accountState ;
+
+accountEmail = "gh@gh.com"
+accountPsw = "3434"
+
+console.table([accountEmail,accountId,accountPsw,accountCity,accountState])
