2c880db8520df1e78cbfa7809ded069b91422d2b
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-10 11:19:23 +0530

    Prefix and postfix operator

---
+++ 01_basics/05_incre&decre.js
@@ -0,0 +1,14 @@
+let a = 10;
+
+console.log(a);
+console.log(a++);//use and then increment
+console.log(a);
+
+
+console.log()
+
+
+let b = 5
+console.log(b)
+console.log(++b)//increase then use
+console.log(b)
