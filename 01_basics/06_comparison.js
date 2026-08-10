433f0d8d69a2347868667b63852fc4b9e21c346a
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-10 19:43:46 +0530

    Checking comparison operators

---
+++ 01_basics/06_comparison.js
@@ -0,0 +1,12 @@
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null <= 0)
+console.log(null > 0)
+// reason: study from notes
+
+console.log()
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+console.log(undefined > 0)
