27e3ba0655514dfe9de0d79e2f876d6fe9bd9cb7
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-17 23:14:19 +0530

    Learning concepts of string and function of string

---
+++ 01_basics/09_String.js
@@ -0,0 +1,10 @@
+const gemeName = newString "Gudiya-Singh"
+
+
+
+const url = "https://gudiya.com/gudiya%90singh"
+console.log(url.replace('%90' , '-'))
+
+console.log(url.includes('govind'))
+
+console.log
