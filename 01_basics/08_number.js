ef7c3c4099dee131498eb2759fd5642408a8ebbd
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-20 22:08:25 +0530

    Concept of numbers and their function in js

---
+++ 01_basics/08_number.js
@@ -0,0 +1,27 @@
+const score = 400
+console.log(score)
+
+const balance = new Number (400)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(typeof balance)
+
+console.log(balance.toFixed(2))
+console.log(balance.toExponential())
+console.log(balance.toString().length)
+console.log(balance.toString().charCodeAt(1))
+console.log(balance.toString().substring)
+console.log(balance.toString().replace('0' , '2'))
+
+const No = 123.8966
+console.log(No.toPrecision(4))
+
+const anotherNo = 1123.8966
+console.log(anotherNo.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'))
+
+
