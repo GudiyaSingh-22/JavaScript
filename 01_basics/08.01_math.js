2d81f7d46bb3d72634a0cc565de6039649e87805
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-22 22:15:46 +0530

    Learning concepts of math in js

---
+++ 01_basics/08.01_math.js
@@ -0,0 +1,21 @@
+console.log(Math)
+console.log(Math.abs(-5))
+console.log(Math.round(5.6))
+console.log(Math.ceil(5.2))
+console.log(Math.floor(5.8))
+console.log(Math.min(5.9))
+
+console.log()
+
+console.log(Math.random()) // gives value b/w 0 to 1
+console.log((Math.random()*10))
+console.log((Math.random()*10) + 1)
+
+
+console.log()
+
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min)) + min)
