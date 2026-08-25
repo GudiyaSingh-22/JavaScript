5a108a05e84f723902de505a565bef197357e8d7
gudiyasingh9140-lang <gudiyasingh9140@gmail.com>
2026-08-25 23:44:15 +0530

    learning arrays and its function in js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,31 @@
+let myArray = [1,2,3,4,5,6]
+console.log(myArray)
+
+//myArray.push(9)
+//console.log(myArray)
+
+// let x = myArray.pop()
+ //console.log(myArray)
+// console.log(x)// value removed by pop can be returned
+
+// console.log(myArray.includes(9))
+// myArray.shift(8)
+// console.log(myArray)
+//myArray.unshift(7)
+//console.log(myArray)
+
+// slice & splice
+
+ console.log("A" , myArray)
+ console.log(myArray.slice(1,3))
+ console.log(myArray)
+
+console.log()
+
+//console.log("B" , myArray)
+ //console.log(myArray.splice(1,3))
+ //console.log(myArray)
+
+ let arr = [1,2,3,4,5]
+ console.log(arr.slice(-4,-1))
+ console.log(arr)
